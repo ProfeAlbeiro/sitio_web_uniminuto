5eb123027f6ac5ea1f6f090366bcb5d8aa243db2
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-04-12 15:37:01 -0500

    worker

---
+++ worker.js
@@ -0,0 +1,8 @@
+if('serviceWorker' in navigator){
+    console.log('Service Worker is available');
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('./dist/serviceWork.js')
+        .then(response=>console.log('Service Worker:' + response))
+        .catch(err=>console.error(err));
+    });
+}
