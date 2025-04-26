baa796a860d7be1c2f64f5469080240f11e98f3a
RSP-L17-LT-025 <RSP-L17-LT-025@UMD.LOCAL.EDU>
2025-04-26 15:10:40 -0500

    Cambio uno

--- worker.js
+++
@@ -1,8 +0,0 @@
-if('serviceWorker' in navigator){
-    console.log('Service Worker es correcto');
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('./dist/serviceWork.js')
-        .then(response=>console.log('Service Worker:' + response))
-        .catch(err=>console.error(err));
-    });
-}
